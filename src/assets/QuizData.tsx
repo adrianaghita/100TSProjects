7ac7047c608bda6809900ba9c698c5ab4fb27932
Adriana Ghita <a.adrianaghita@gmail.com>
2024-02-27 19:47:23 +0200

    Rename project title

--- src/assets/QuizData.tsx
+++ src/assets/QuizData.tsx
@@ -1,11 +1,10 @@
 export const quizData = [
   {
-    question:
-      "Which branch of psychology focuses on understanding individual behavior in social contexts and groups?",
-    option1: "Social psychology",
-    option2: "Developmental psychology",
-    option3: "Clinical psychology",
-    option4: "Cognitive psychology",
+    question: "Who painted the Mona Lisa?",
+    option1: "Leonardo da Vinci",
+    option2: "Vincent van Gogh",
+    option3: "Pablo Picasso",
+    option4: "Michelangelo",
     ans: 1,
   },
   {
@@ -18,30 +17,27 @@ export const quizData = [
     ans: 4,
   },
   {
-    question:
-      "Which type of reinforcement involves the removal of an unpleasant stimulus to increase the likelihood of a behavior recurring?",
-    option1: "Positive reinforcement",
-    option2: "Negative reinforcement",
-    option3: "Punishment",
-    option4: "Extinction",
+    question: `Who wrote "Romeo and Juliet"?`,
+    option1: "Charles Dickens",
+    option2: "William Shakespeare",
+    option3: "Jane Austen",
+    option4: "Mark Twain",
     ans: 2,
   },
   {
-    question:
-      "Which neurotransmitter is associated with feelings of pleasure and reward?",
-    option1: "Serotonin",
-    option2: "Dopamine",
-    option3: "Acetylcholine",
-    option4: "GABA (Gamma-Aminobutyric Acid)",
+    question: "What is the tallest mountain in the world?",
+    option1: "Mount Kilimanjaro",
+    option2: "Mount Everest",
+    option3: "Mount Fuji",
+    option4: "K2",
     ans: 2,
   },
   {
-    question:
-      "What is the term for the process of encoding, storing, and retrieving information from memory?",
-    option1: "Perception",
-    option2: "Memory consolidation",
-    option3: "Cognition",
-    option4: "Learning",
+    question: "What is the largest mammal in the world?",
+    option1: "Elephant",
+    option2: "Giraffe",
+    option3: "Blue whale",
+    option4: "Hippopotamus",
     ans: 3,
   },
 ];
